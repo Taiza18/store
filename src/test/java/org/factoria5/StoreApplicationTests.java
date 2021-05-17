fd88b9a284fb61d0dd46a1c5e93c6c7538b78b15
Taiza18 <taizabrito@hotmail.com>
2021-05-17 09:36:13 +0200

    Initial commit

---
+++ src/test/java/org/factoria5/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoria5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
